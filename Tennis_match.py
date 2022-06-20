6003ffd4b406ae2818c77b2a4871ae0ea7358e5f
rokfiser12345 <73073652+rokfiser12345@users.noreply.github.com>
2022-06-20 09:40:21 +0200

    pull
    Wrong author

--- Tennis_match.py
+++ Tennis_match.py
@@ -56,7 +56,7 @@ class Tennis_match:
         return str
 
     def coin_toss(self):
-        if random.uniform(0,1) <= 0.5:
+        if random.uniform(0,1) < 0.5:
             print(f'\n PlayerB has won the coin toss! \n')
             self.set_num[1] = self.playerB
             self.server = self.playerB
@@ -64,32 +64,101 @@ class Tennis_match:
         else:
             print(f'\nPlayerA has won the coin toss!\n')
 
+    def sim_point(self):
+        if self.server.winsPoint():
+            self.server.inc_points()
+        else:
+            self.reciever.inc_points()
+
     def sim_game(self):
         while not self.GameisOver():
-            if self.server.winsPoint():
-                self.server.inc_points()
-            else:
-                self.reciever.inc_points()
-        # return who wins game to player object
+            self.sim_point()
+        # increment the game total for winner of game
         a, b = self.getPoints()
         if a > b:
             self.playerA.inc_games()
         else:
             self.playerB.inc_games()
+        
+    def sim_set(self):
+        while not self.SetisOver():
+            self.sim_game()
+            self.print_game_score()
+            self.resetPoints()
+            self.change_server()
+        # if games are tied at 6-6 play tiebreaker
+        a, b = self.getGames()
+        if a == b:
+            self.TiebreakGame()
+        # increment the set total for winner of set
+        a, b = self.getGames()
+        if a > b:
+            self.playerA.inc_set()
+        else:
+            self.playerB.inc_set()
+        print('---------------------------------')
 
-    def nextSet(self):
-        self.set_num[0] += 1
-
-    def Set_num(self):
-        return self.set_num[0]
+    def TiebreakGame(self):
+        self.sim_point() # the first player has the first serve
+        self.change_server()
+        if self.set_num[0] < 5:
+            while not self.tiebreakGameOver(): 
+                self.sim_point() # the next player has the next two serves
+                if not self.tiebreakGameOver(): # the second serve of a player in a tiebreak
+                    self.sim_point() 
+                    self.change_server()
+        else:
+            while not self.tiebreakFinal(): 
+                self.sim_point() # the next player has the next two serves
+                if not self.tiebreakFinal(): # the second serve of a player in a tiebreak
+                    self.sim_point() 
+                    self.change_server()
             
+        a, b = self.getPoints()
+        if a > b:
+            self.playerA.inc_games()
+        else:
+            self.playerB.inc_games()
+        #print result of tiebreak
+        game_a,game_b = self.getGames()
+        print(f'A {a}:{b} B ==> {game_a}:{game_b} TIEBREAK')
+            
+    def play(self):
+        while not self.matchOver():
+            self.nextSet()
+            self.sim_set()
+            self.resetGames()
+            self.swapFirstServe()
+
     def GameisOver(self):
         # The winner of a game is the first player to reach 4 point while the opponent has less than
         a,b = self.getPoints()
-        return a > 3 and a - b >= 2 or b > 3 and b - a >= 2
+        return (a > 3 and a - b >= 2) or (b > 3 and b - a >= 2)
+
+    def tiebreakGameOver(self):
+        a,b = self.getPoints()
+        return (a > 6 and a - b >= 2) or (b > 6 and b - a >= 2)
+
+    def tiebreakFinal(self):
+        a,b = self.getPoints()
+        return (a >= 10 and a - b >= 2) or (b >= 10 and b - a >= 2)
+
+    def SetisOver(self):
+        a,b = self.getGames()
+        return (a >= 6 and a - b >= 2) or (b >= 6 and b - a >= 2) or (a == 6 and b == 6) #the last option leads to a tiebreaker game
+
+    def matchOver(self):
+        a,b = self.getSets()
+        return a == 3 or b == 3
 
     def getPoints(self):
         return self.playerA.get_points(), self.playerB.get_points()
+    
+    def getGames(self):
+        return self.playerA.get_games(), self.playerB.get_games()
+
+    def getSets(self):
+        return self.playerA.get_set(), self.playerB.get_set()
 
     def resetPoints(self):
         self.playerA.reset_players_points(), self.playerB.reset_players_points()
@@ -97,6 +166,15 @@ class Tennis_match:
     def resetGames(self):
         self.playerA.reset_players_games(), self.playerB.reset_players_games()
 
+    def nextSet(self):
+        self.set_num[0] += 1
+
+    def print_game_score(self):
+        points_a,points_b = self.getPoints()
+        game_a,game_b = self.getGames()
+        server = self.getServer()
+        print(f'A {points_a}:{points_b} B ==> {game_a}:{game_b} it was {server} to serve')
+        
     def change_server(self):
         # Change who has the serve and who recieves the serve
         if self.server == self.playerA:
@@ -105,54 +183,6 @@ class Tennis_match:
         else:
             self.server = self.playerA
             self.reciever = self.playerB
-
-    def sim_set(self):
-        while not self.SetisOver():
-            self.sim_game()
-            self.print_game_score()
-            self.resetPoints()
-            self.change_server()
-        a, b = self.getGames()
-        if a > b:
-            self.playerA.inc_set()
-        else:
-            self.playerB.inc_set()
-        print('---------------------------------')
-
-    def SetisOver(self):
-        a,b = self.getGames()
-        return a > 5 and a-b >= 2 or b > 5 and b - a >= 2
-    
-    def getGames(self):
-        return self.playerA.get_games(), self.playerB.get_games()
-
-    def print_game_score(self):
-        points_a,points_b = self.getPoints()
-        game_a,game_b = self.getGames()
-        server = self.getServer()
-        # Tennis_scores = {0:'0', 1: '15', 2: '30', 3:'40'}
-        # if points_a > points_b:
-        #     points_a = Tennis_scores.get(points_a, 'adv')
-        #     points_b = Tennis_scores.get(points_b, '40')
-        # else:
-        #     points_a = Tennis_scores.get(points_a, '40')
-        #     points_b = Tennis_scores.get(points_b, 'adv')
-            
-        print(f'A {points_a}:{points_b} B ==> {game_a}:{game_b} it was {server} to serve')
-
-    def getSets(self):
-        return self.playerA.get_set(), self.playerB.get_set()
-
-    def matchOver(self):
-        a,b = self.getSets()
-        return a > 2 or b > 2
-
-    def play(self):
-        while not self.matchOver():
-            self.nextSet()
-            self.sim_set()
-            self.resetGames()
-            self.swapFirstServe()
 
     def getServer(self):
         if self.server == self.playerA:
@@ -194,4 +224,4 @@ def main():
     output(Game)
 
 if __name__ == '__main__':
-    main()
+     main()
